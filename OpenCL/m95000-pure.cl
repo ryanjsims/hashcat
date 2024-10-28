9b06f95f4c073e8cbbed1aa82a187624ff52ea59
Ryan Sims <ryanjsims@email.arizona.edu>
2024-10-27 17:18:59 -0700

    Very slow murmur64a implementations

---
+++ OpenCL/m95000-pure.cl
@@ -0,0 +1,129 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+//#define NEW_SIMD_CODE
+
+#ifdef KERNEL_STATIC
+#include M2S(INCLUDE_PATH/inc_vendor.h)
+#include M2S(INCLUDE_PATH/inc_types.h)
+#include M2S(INCLUDE_PATH/inc_platform.cl)
+#include M2S(INCLUDE_PATH/inc_common.cl)
+#include M2S(INCLUDE_PATH/inc_scalar.cl)
+#endif
+
+#define COMPARE_M M2S(INCLUDE_PATH/inc_comp_multi.cl)
+
+#define SHIFTS 47
+
+typedef struct murmur64a_tmp
+{
+  u64 password_buf[32];
+  u32 byte_length;
+
+} murmur64a_tmp_t;
+
+KERNEL_FQ void m95000_init (KERN_ATTR_TMPS (murmur64a_tmp_t))
+{
+  /**
+   * modifier
+   */
+
+  const u64 lid = get_local_id (0);
+  const u64 gid = get_global_id (0);
+
+  if (gid >= GID_CNT) return;
+
+  u32 in[64];
+
+  for (int i = 0; i < 64; i++)
+  {
+    in[i] = pws[gid].i[i];
+  }
+
+  u32 password_length = pws[gid].pw_len;
+
+  u64 out[32];
+  for(int i = 0; i < 32; i++)
+  {
+    out[i] = 0;
+  }
+
+  PRIVATE_AS u8 *in_ptr  = (PRIVATE_AS u8 *) in;
+  PRIVATE_AS u8 *out_ptr = (PRIVATE_AS u8 *) out;
+
+  for(int i = 0; i < password_length; i++)
+  {
+    out_ptr[i] = in_ptr[i];
+  }
+
+  for(int i = 0; i < 32; i++)
+  {
+    tmps[gid].password_buf[i] = out[i];
+  }
+  tmps[gid].byte_length = password_length;
+}
+
+KERNEL_FQ void m95000_loop (KERN_ATTR_TMPS (murmur64a_tmp_t))
+{
+
+}
+
+KERNEL_FQ void m95000_comp (KERN_ATTR_TMPS (murmur64a_tmp_t))
+{
+  const u64 lid = get_local_id (0);
+  const u64 gid = get_global_id (0);
+
+  if (gid >= GID_CNT) return;
+
+  const u64 seed = 0;
+  const u64 mix = 0xc6a4a7935bd1e995;
+
+  u64 hash = seed ^ ((u64)tmps[gid].byte_length * mix);
+
+  int i;
+  // printf("\n");
+  for (i = 0; tmps[gid].byte_length - i >= 8; i += 8)
+  {
+    u64 key = tmps[gid].password_buf[i / 8];
+
+    key *= mix;
+    key ^= key >> SHIFTS;
+    key *= mix;
+
+    hash ^= key;
+    hash *= mix;
+
+    // printf("i = % 3d  hash = %08x%08x  key = %08x%08x\n", i, h32_from_64(hash), l32_from_64(hash), h32_from_64(key), l32_from_64(key));
+  }
+
+  if (tmps[gid].byte_length - i > 0) {
+    GLOBAL_AS const u8 *password_bytes = ((GLOBAL_AS const u8 *) tmps[gid].password_buf) + i;
+    for (int j = tmps[gid].byte_length - i - 1; j >= 0; j--)
+    {
+      hash ^= (u64)password_bytes[j] << (8 * j);
+      // printf("j = % 3d  hash = %08x%08x\n", j, h32_from_64(hash), l32_from_64(hash));
+    }
+    hash *= mix;
+    // printf("i = % 3d  hash = %08x%08x\n", i, h32_from_64(hash), l32_from_64(hash));
+  }
+
+  hash ^= hash >> SHIFTS;
+
+  hash *= mix;
+  hash ^= hash >> SHIFTS;
+
+
+  const u32 r0 = l32_from_64(hash);
+  const u32 r1 = h32_from_64(hash);
+  const u32 r2 = 0;
+  const u32 r3 = 0;
+  // printf("hash = %08x%08x\n", r1, r0);
+
+  #define il_pos 0
+
+  #ifdef KERNEL_STATIC
+  #include COMPARE_M
+  #endif
+}
---
+++ src/modules/module_95001.c
@@ -0,0 +1,272 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#include "common.h"
+#include "types.h"
+#include "modules.h"
+#include "bitops.h"
+#include "convert.h"
+#include "shared.h"
+
+static const u32   ATTACK_EXEC    = ATTACK_EXEC_OUTSIDE_KERNEL;
+static const u32   DGST_POS0      = 0;
+static const u32   DGST_POS1      = 1;
+static const u32   DGST_POS2      = 2;
+static const u32   DGST_POS3      = 3;
+static const u32   DGST_SIZE      = DGST_SIZE_8_2;
+static const u32   HASH_CATEGORY  = HASH_CATEGORY_RAW_CHECKSUM;
+static const char *HASH_NAME      = "MurmurHash64a";
+static const u64   KERN_TYPE      = 95001;
+static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE 
+                                  | OPTI_TYPE_USES_BITS_64;
+static const u64   OPTS_TYPE      = OPTS_TYPE_PT_GENERATE_LE
+                                  | OPTS_TYPE_SUGGEST_KG;
+static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
+static const char *ST_PASS        = "ashcathashcathashcat";
+static const char *ST_HASH        = "9fcfd9e36c9a1474:hashcath";
+
+typedef struct murmur64a_tmp
+{
+  u64 password_buf[32];
+  u32 byte_length;
+  u64 prefix_hash;
+
+} murmur64a_tmp_t;
+
+u32         module_attack_exec    (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ATTACK_EXEC;     }
+u32         module_dgst_pos0      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS0;       }
+u32         module_dgst_pos1      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS1;       }
+u32         module_dgst_pos2      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS2;       }
+u32         module_dgst_pos3      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS3;       }
+u32         module_dgst_size      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_SIZE;       }
+u32         module_hash_category  (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return HASH_CATEGORY;   }
+const char *module_hash_name      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return HASH_NAME;       }
+u64         module_kern_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return KERN_TYPE;       }
+u32         module_opti_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return OPTI_TYPE;       }
+u64         module_opts_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return OPTS_TYPE;       }
+u32         module_salt_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return SALT_TYPE;       }
+const char *module_st_hash        (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ST_HASH;         }
+const char *module_st_pass        (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ST_PASS;         }
+
+// u32 module_salt_max (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+// {
+//   const u32 salt_max = 0;
+
+//   return salt_max;
+// }
+
+// u32 module_salt_min (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+// {
+//   const u32 salt_min = 0;
+
+//   return salt_min;
+// }
+
+u64 module_tmp_size (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+{
+  const u64 tmp_size = (const u64) sizeof (murmur64a_tmp_t);
+
+  return tmp_size;
+}
+
+int generate_intermediate_value(const u8 *prefix, u32 prefix_len, u32 suffix_len, u64* iv_out)
+{
+  if(prefix_len % 8 != 0)
+  {
+    fprintf(stderr, "Prefix length mod 8 must be equal to 0! %d %% 8 == %d\nUse a rule to prepend the last %d characters of the prefix to the password\n", prefix_len, prefix_len % 8, prefix_len % 8);
+    return -1;
+  }
+
+  const u32 shifts = 47;
+
+  u64 iv_len = (u64)(prefix_len + suffix_len);
+
+  u64* data_buf = (u64*)prefix;
+
+  u64 seed = 0;
+  const u64 mix = 0xc6a4a7935bd1e995;
+
+  u64 hash = seed ^ (iv_len * mix);
+
+  for(int i = 0; prefix_len - i >= 8; i += 8)
+  {
+    u64 key = data_buf[i / 8];
+
+    key *= mix;
+    key ^= key >> shifts;
+    key *= mix;
+
+    hash ^= key;
+    hash *= mix;
+  }
+
+  *iv_out = hash;
+  return 0;
+}
+
+int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED void *digest_buf, MAYBE_UNUSED salt_t *salt, MAYBE_UNUSED void *esalt_buf, MAYBE_UNUSED void *hook_salt_buf, MAYBE_UNUSED hashinfo_t *hash_info, const char *line_buf, MAYBE_UNUSED const int line_len)
+{
+  u64 *digest = (u64 *) digest_buf;
+  u64 *salt_data = (u64 *) salt->salt_buf;
+
+  hc_token_t token;
+
+  memset (&token, 0, sizeof (hc_token_t));
+
+  token.token_cnt = 2;
+
+  token.sep[0]     = hashconfig->separator;
+  token.len[0]     = 16;
+  token.attr[0]    = TOKEN_ATTR_FIXED_LENGTH
+                   | TOKEN_ATTR_VERIFY_HEX;
+
+  token.sep[1]     = hashconfig->separator;
+  token.len_min[1] = 8;
+  token.len_max[1] = 248;
+  token.attr[1]    = TOKEN_ATTR_VERIFY_LENGTH;
+
+  const int rc_tokenizer = input_tokenizer ((const u8 *) line_buf, line_len, &token);
+
+  if (rc_tokenizer != PARSER_OK) return (rc_tokenizer);
+
+  // digest
+
+  const u8 *hash_pos = token.buf[0];
+
+  // printf("hash_pos: %8s\n", hash_pos);
+
+  digest[0] = hex_to_u64(&hash_pos[0]);
+  digest[1] = 0;
+
+  // printf("digest[0] = %016"PRIx64"\n", digest[0]);
+
+  digest[0] = byte_swap_64(digest[0]);
+
+  // printf("digest[0] = %016"PRIx64"\n", digest[0]);
+
+  const u8 *salt_pos = token.buf[1];
+  const int salt_len = token.len[1];
+
+  for(u32 i = 0; i < 32; i++) {
+    int rc = generate_intermediate_value(salt_pos, salt_len, (i + 4) * 2, &salt_data[i]);
+    if(rc == -1)
+    {
+      return (PARSER_SALT_LENGTH);
+    }
+    // printf("salt_data[%d] = %016"PRIx64" (len %d)\n", i, salt_data[i], (i + 4) * 2);
+  }
+  
+  salt->salt_len = 256;
+
+  memcpy(salt->salt_buf_pc, salt_pos, salt_len);
+
+  salt->salt_len_pc = salt_len;
+
+  return (PARSER_OK);
+}
+
+int module_hash_encode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const void *digest_buf, MAYBE_UNUSED const salt_t *salt, MAYBE_UNUSED const void *esalt_buf, MAYBE_UNUSED const void *hook_salt_buf, MAYBE_UNUSED const hashinfo_t *hash_info, char *line_buf, MAYBE_UNUSED const int line_size)
+{
+  const u64 *digest = (const u64 *) digest_buf;
+
+  const int line_len = snprintf (line_buf, line_size, "%016"PRIx64":%.*s", digest[0], salt->salt_len_pc, (char*)salt->salt_buf_pc);
+  // printf("line_buf = \"%.*s\"\n", line_len, line_buf);
+
+  return line_len;
+}
+
+u32 module_pw_min (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+{
+  const u32 pw_min = 0;
+
+  return pw_min;
+}
+
+u32 module_pw_max (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+{
+  const u32 pw_max = 70;
+
+  return pw_max;
+}
+
+void module_init (module_ctx_t *module_ctx)
+{
+  module_ctx->module_context_size             = MODULE_CONTEXT_SIZE_CURRENT;
+  module_ctx->module_interface_version        = MODULE_INTERFACE_VERSION_CURRENT;
+
+  module_ctx->module_attack_exec              = module_attack_exec;
+  module_ctx->module_benchmark_esalt          = MODULE_DEFAULT;
+  module_ctx->module_benchmark_hook_salt      = MODULE_DEFAULT;
+  module_ctx->module_benchmark_mask           = MODULE_DEFAULT;
+  module_ctx->module_benchmark_charset        = MODULE_DEFAULT;
+  module_ctx->module_benchmark_salt           = MODULE_DEFAULT;
+  module_ctx->module_build_plain_postprocess  = MODULE_DEFAULT;
+  module_ctx->module_deep_comp_kernel         = MODULE_DEFAULT;
+  module_ctx->module_deprecated_notice        = MODULE_DEFAULT;
+  module_ctx->module_dgst_pos0                = module_dgst_pos0;
+  module_ctx->module_dgst_pos1                = module_dgst_pos1;
+  module_ctx->module_dgst_pos2                = module_dgst_pos2;
+  module_ctx->module_dgst_pos3                = module_dgst_pos3;
+  module_ctx->module_dgst_size                = module_dgst_size;
+  module_ctx->module_dictstat_disable         = MODULE_DEFAULT;
+  module_ctx->module_esalt_size               = MODULE_DEFAULT;
+  module_ctx->module_extra_buffer_size        = MODULE_DEFAULT;
+  module_ctx->module_extra_tmp_size           = MODULE_DEFAULT;
+  module_ctx->module_extra_tuningdb_block     = MODULE_DEFAULT;
+  module_ctx->module_forced_outfile_format    = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_count        = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_parse        = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_save         = MODULE_DEFAULT;
+  module_ctx->module_hash_decode_postprocess  = MODULE_DEFAULT;
+  module_ctx->module_hash_decode_potfile      = MODULE_DEFAULT;
+  module_ctx->module_hash_decode_zero_hash    = MODULE_DEFAULT;
+  module_ctx->module_hash_decode              = module_hash_decode;
+  module_ctx->module_hash_encode_status       = MODULE_DEFAULT;
+  module_ctx->module_hash_encode_potfile      = MODULE_DEFAULT;
+  module_ctx->module_hash_encode              = module_hash_encode;
+  module_ctx->module_hash_init_selftest       = MODULE_DEFAULT;
+  module_ctx->module_hash_mode                = MODULE_DEFAULT;
+  module_ctx->module_hash_category            = module_hash_category;
+  module_ctx->module_hash_name                = module_hash_name;
+  module_ctx->module_hashes_count_min         = MODULE_DEFAULT;
+  module_ctx->module_hashes_count_max         = MODULE_DEFAULT;
+  module_ctx->module_hlfmt_disable            = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_size    = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_init    = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_term    = MODULE_DEFAULT;
+  module_ctx->module_hook12                   = MODULE_DEFAULT;
+  module_ctx->module_hook23                   = MODULE_DEFAULT;
+  module_ctx->module_hook_salt_size           = MODULE_DEFAULT;
+  module_ctx->module_hook_size                = MODULE_DEFAULT;
+  module_ctx->module_jit_build_options        = MODULE_DEFAULT;
+  module_ctx->module_jit_cache_disable        = MODULE_DEFAULT;
+  module_ctx->module_kernel_accel_max         = MODULE_DEFAULT;
+  module_ctx->module_kernel_accel_min         = MODULE_DEFAULT;
+  module_ctx->module_kernel_loops_max         = MODULE_DEFAULT;
+  module_ctx->module_kernel_loops_min         = MODULE_DEFAULT;
+  module_ctx->module_kernel_threads_max       = MODULE_DEFAULT;
+  module_ctx->module_kernel_threads_min       = MODULE_DEFAULT;
+  module_ctx->module_kern_type                = module_kern_type;
+  module_ctx->module_kern_type_dynamic        = MODULE_DEFAULT;
+  module_ctx->module_opti_type                = module_opti_type;
+  module_ctx->module_opts_type                = module_opts_type;
+  module_ctx->module_outfile_check_disable    = MODULE_DEFAULT;
+  module_ctx->module_outfile_check_nocomp     = MODULE_DEFAULT;
+  module_ctx->module_potfile_custom_check     = MODULE_DEFAULT;
+  module_ctx->module_potfile_disable          = MODULE_DEFAULT;
+  module_ctx->module_potfile_keep_all_hashes  = MODULE_DEFAULT;
+  module_ctx->module_pwdump_column            = MODULE_DEFAULT;
+  module_ctx->module_pw_max                   = module_pw_max;
+  module_ctx->module_pw_min                   = module_pw_min;
+  module_ctx->module_salt_max                 = MODULE_DEFAULT;
+  module_ctx->module_salt_min                 = MODULE_DEFAULT;
+  module_ctx->module_salt_type                = module_salt_type;
+  module_ctx->module_separator                = MODULE_DEFAULT;
+  module_ctx->module_st_hash                  = module_st_hash;
+  module_ctx->module_st_pass                  = module_st_pass;
+  module_ctx->module_tmp_size                 = module_tmp_size;
+  module_ctx->module_unstable_warning         = MODULE_DEFAULT;
+  module_ctx->module_warmup_disable           = MODULE_DEFAULT;
+}
